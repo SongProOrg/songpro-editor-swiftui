84f86cd32f8b78c85b3e56bf995555a255b25e35
Brian Kelly <polymonic@gmail.com>
2020-06-29 07:35:24 -0500

    Initial Commit

---
+++ SongPro Again/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SongPro Again
+//
+//  Created by Brian Kelly on 6/29/20.
+//  Copyright © 2020 SongPro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SongPro Again/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SongPro Again
+//
+//  Created by Brian Kelly on 6/29/20.
+//  Copyright © 2020 SongPro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
