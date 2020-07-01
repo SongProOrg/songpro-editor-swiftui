d46b372b274f614de6dcfda4524638a15061ef34
Brian Kelly <polymonic@gmail.com>
2020-07-01 08:32:12 -0500

    Extract previewSong from previews.

--- SongPro Editor/EditorView.swift
+++ SongPro Editor/EditorView.swift
@@ -45,29 +45,7 @@ struct EditorView: NSViewRepresentable {
 }
 
 struct EditorView_PreviewContainer: View {
-    @State var text = """
-@title=Escape Capsule
-@artist=Brian Kelly
-!bandcamp=https://spilth.bandcamp.com/track/escape-capsule
-
-# Verse 1
-
-Climb a-[D]board [A]
-I've been [Bm]waiting for you [F#m]
-Climb a-[G]board [D]
-You'll be [Asus4]safe in [A7]here
-
-# Chorus 1
-
-[G] I'm a [D]rocket [F#]made for your pro-[Bm]tection
-You're [G]safe with me, un-[A]til you leave
-
-# Solo
-
-| [Em] | [D] | [Em] | [D] |
-| [Em] | [D] | [Em] | [F#] |
-| [B] | [B] | [Bm] | [Bm] |
-"""
+    @State var text = previewSong
 
     var body: some View {
         EditorView(text: $text)
@@ -78,11 +56,11 @@ struct EditorView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             EditorView_PreviewContainer()
-                .environment(\.colorScheme, .light)
-                .previewDisplayName("Light Mode")
+                    .environment(\.colorScheme, .light)
+                    .previewDisplayName("Light Mode")
             EditorView_PreviewContainer()
-                .environment(\.colorScheme, .dark)
-                .previewDisplayName("Dark Mode")
+                    .environment(\.colorScheme, .dark)
+                    .previewDisplayName("Dark Mode")
         }
     }
 }
--- SongPro Editor/HtmlView.swift
+++ SongPro Editor/HtmlView.swift
@@ -199,29 +199,7 @@ struct HtmlView: NSViewRepresentable {
 }
 
 struct HtmlView_PreviewContainer: View {
-    @State var text = """
-@title=Escape Capsule
-@artist=Brian Kelly
-!bandcamp=https://spilth.bandcamp.com/track/escape-capsule
-
-# Verse 1
-
-Climb a-[D]board [A]
-I've been [Bm]waiting for you [F#m]
-Climb a-[G]board [D]
-You'll be [Asus4]safe in [A7]here
-
-# Chorus 1
-
-[G] I'm a [D]rocket [F#]made for your pro-[Bm]tection
-You're [G]safe with me, un-[A]til you leave
-
-# Solo
-
-| [Em] | [D] | [Em] | [D] |
-| [Em] | [D] | [Em] | [F#] |
-| [B] | [B] | [Bm] | [Bm] |
-"""
+    @State var text = previewSong
 
     var body: some View {
         HtmlView(text: $text)
@@ -232,11 +210,11 @@ struct HtmlView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             HtmlView_PreviewContainer()
-                .environment(\.colorScheme, .light)
-                .previewDisplayName("Light Mode")
+                    .environment(\.colorScheme, .light)
+                    .previewDisplayName("Light Mode")
             HtmlView_PreviewContainer()
-                .environment(\.colorScheme, .dark)
-                .previewDisplayName("Dark Mode")
+                    .environment(\.colorScheme, .dark)
+                    .previewDisplayName("Dark Mode")
         }
     }
 }
